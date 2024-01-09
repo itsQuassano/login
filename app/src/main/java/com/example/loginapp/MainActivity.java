9633d60b6ef8b11ac1822c0f9c06913b1757de0d
its64_quassano <utkarshphy64@gmail.com>
2024-01-10 01:36:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.loginapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.Login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username: " + username + " and Password: " + password);
+            }
+        });
+    }
+}
